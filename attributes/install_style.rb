78aa141ef218298820ad2eb17b6e854d017b839a
Taylor Monacelli <taylormonacelli@gmail.com>
2017-05-15 09:26:46 -0700

    clean path

--- attributes/install_style.rb
+++ attributes/install_style.rb
@@ -2,7 +2,7 @@ default['scollector']['release_url'] = 'https://github.com/bosun-monitor/bosun/r
 default['scollector']['version']     = '0.4.0'
 default['scollector']['bin_path']    = case node['platform']
                                        when 'windows'
-                                         '"C:\\Program Files\\StackExchange\\scollector\\scollector.exe"'
+                                         "#{ENV['ProgramFiles']}\\StackExchange\\scollector\\scollector.exe"
                                        else
                                          '/usr/local/bin/scollector'
                                        end
